dff7872b032bf2caef795180d4056bf628d25896
willychen <willy77423@gmail.com>
2025-03-08 12:11:05 +0800

    預設網址改yuLogin

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { MainComponent } from './direct-page/main/main.component';
 import { YumeyuLoginComponent } from './direct-page/yumeyu-login/yumeyu-login.component';
 
 export const routes: Routes = [
-  { path: '', component: MainComponent }, // 設定根路徑的首頁
+  { path: '', component: YumeyuLoginComponent }, // 設定根路徑的首頁
   { path: 'login', component: MainComponent }, // 設定 /login 路由，顯示 MainComponent
   { path: 'yulogin', component: YumeyuLoginComponent }, // 設定 /yulogin 路由，顯示 YumeyuLoginComponent
 ];
