6f0565aaa95d9512047ab710a4ca303f61ab32dd
willychen <willy77423@gmail.com>
2025-03-08 11:53:44 +0800

    雙首頁設定

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { MainComponent } from './direct-page/main/main.component';
+import { YumeyuLoginComponent } from './direct-page/yumeyu-login/yumeyu-login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: MainComponent }, // 設定根路徑的首頁
+  { path: 'login', component: MainComponent }, // 設定 /login 路由，顯示 MainComponent
+  { path: 'yulogin', component: YumeyuLoginComponent }, // 設定 /yulogin 路由，顯示 YumeyuLoginComponent
+];
