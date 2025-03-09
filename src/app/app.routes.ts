0c818f815c86e41c0037b2c3d6998c899faa8604
willychen <willy77423@gmail.com>
2025-03-09 16:57:10 +0800

    預設開啟appcomponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import { MainComponent } from './direct-page/main/main.component';
 import { YumeyuLoginComponent } from './direct-page/yumeyu-login/yumeyu-login.component';
+import { AppComponent } from './app.component';
 
 export const routes: Routes = [
-  { path: '', component: YumeyuLoginComponent }, // 設定根路徑的首頁
+  { path: '', component: AppComponent }, // 設定根路徑的首頁
   { path: 'login', component: MainComponent }, // 設定 /login 路由，顯示 MainComponent
   { path: 'yu', component: YumeyuLoginComponent }, // 設定 /yulogin 路由，顯示 YumeyuLoginComponent
 ];
