d7d181ef0c5a37c4aafdcabc157ea3dac915c695
willychen <willy77423@gmail.com>
2025-03-22 23:59:47 +0800

    變更預設首頁

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,25 +7,28 @@ export const routes: Routes = [
         // component: MainPageComponent,
         loadComponent: () => import('./direct-page/main-page/main-page.component').then(m=>m.MainPageComponent),
         loadChildren: () => import('./direct-page/main-page/main-routing.module').then(m=>m.MainRoutingModule),
+        pathMatch: 'prefix'
     },
     { 
         path: 'yu',
         // component: YumeyuMainPageComponent,
         loadComponent: () => import('./direct-page/yumeyu-main-page/yumeyu-main-page.component').then(m=>m.YumeyuMainPageComponent),
         loadChildren: () => import('./direct-page/yumeyu-main-page/yumeyu-routing.module').then(m=>m.YumeyuRoutingModule),
+        pathMatch: 'prefix'
     },
     { 
         path: 'error',
         // component: ErrorPageComponent,
         loadComponent: () => import('./direct-page/error-page/error-page.component').then(m=>m.ErrorPageComponent),
+        pathMatch: 'prefix'
     },    
     { 
         path: '',
         // component: MainPageComponent,
-        loadComponent: () => import('./direct-page/main-page/main-page.component').then(m=>m.MainPageComponent),
-        loadChildren: () => import('./direct-page/main-page/main-routing.module').then(m=>m.MainRoutingModule),
+        loadComponent: () => import('./direct-page/yumeyu-main-page/yumeyu-main-page.component').then(m=>m.YumeyuMainPageComponent),
+        loadChildren: () => import('./direct-page/yumeyu-main-page/yumeyu-routing.module').then(m=>m.YumeyuRoutingModule),
     },
     { path: 'login', component: MainPageComponent },  // 設定 /login 路由，顯示 MainPageComponent
-    { path: '**', redirectTo: 'error' },
+    { path: '**', redirectTo: 'error', pathMatch: 'prefix' },
     
 ];
